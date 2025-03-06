f7f61b0ed6910e4088a1ac728d24e0d1ebbc4aff
subhangaK <subhangakhanal@gmail.com>
2025-03-06 19:36:54 +0545

    For Deployment changing allowed origins

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,11 +33,10 @@ export const register = async (req, res) => {
     );
 
     res.cookie("token", token, {
-      httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-      secure: true, // Ensures cookies are only sent over HTTPS
-      sameSite: "none", // Allows cross-site cookies
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-      domain: ".onrender.com", // Allow cookies to be shared across subdomains
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     // Sending welcome email
